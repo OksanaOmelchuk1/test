4e0b86908d5bb382ca541ac66a3e4efac95af6ef
Oksana Omelchuk <oksana22022001@gmail.com>
2022-07-24 21:00:22 +0300

    update

---
+++ js.js
@@ -0,0 +1,2 @@
+cv.addEventListener('mouseenter',()=> document.getElementById("imgs").classList.add('bl'));
+cv.addEventListener('mouseleave',()=> document.getElementById("imgs").classList.remove('bl'));
